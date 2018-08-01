eef20a82f1f9b458ce1640db612d397996c16368
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-01 14:22:23 +0200

    Create 'GameReviewCreateView'

--- src/reviews/api/urls.py
+++ src/reviews/api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import GameReviewRudView
+from .views import GameReviewRudView, GameReviewCreateView
 
 app_name = 'api-reviews'
 
 urlpatterns = [
+    path('create', GameReviewCreateView.as_view(), name='review-create'),
     path('<int:pk>', GameReviewRudView.as_view(), name='review-rud'),
 ]
--- src/reviews/api/views.py
+++ src/reviews/api/views.py
@@ -2,6 +2,16 @@ from rest_framework import generics
 from reviews.models import GameReview
 from .serializers import GameReviewSerializer
 
+class GameReviewCreateView(generics.CreateAPIView):
+    lookup_field = 'pk'
+    serializer_class = GameReviewSerializer
+    
+    def get_queryset(self):
+        return GameReview.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
 class GameReviewRudView(generics.RetrieveUpdateDestroyAPIView):
     lookup_field = 'pk'
     serializer_class = GameReviewSerializer
