dfdbc7150aa3cdd62a6ed29ec8fe351511b4ebc7
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-01 14:12:44 +0200

    Add title validation

--- src/reviews/api/serializers.py
+++ src/reviews/api/serializers.py
@@ -12,3 +12,12 @@ class GameReviewSerializer(serializers.ModelSerializer):
             'content',
             'timestamp',
         ]
+        read_only_fields = ['user']
+
+    def validate_title(self, value):
+        qs = GameReview.objects.filter(title__iexact=value)
+        if self.instance:
+            qs = qs.exclude(pk=self.instance.pk)
+        if qs.exists():
+            raise serializers.ValidationError('This title has already been used')
+        return value
