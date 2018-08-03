8de5b1f824b5297fba0a4a63528dee89377025de
Robert Klim <kontakt.robert.klim@gmail.com>
2018-08-03 17:24:00 +0200

    Tests for authenticated user

--- src/game_reviews_api/settings.py
+++ src/game_reviews_api/settings.py
@@ -125,7 +125,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ),
-    'DEFAULT_aUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
--- src/reviews/api/tests.py
+++ src/reviews/api/tests.py
@@ -2,10 +2,14 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.reverse import reverse as api_reverse
 from rest_framework.test import APITestCase
+from rest_framework_jwt.settings import api_settings
 
 from reviews.models import GameReview
 
 User = get_user_model()
+
+payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 class GameReviewAPITestCase(APITestCase):
     def setUp(self):
@@ -32,11 +36,11 @@ class GameReviewAPITestCase(APITestCase):
         response = self.client.get(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_post_list(self):
+    def test_post_item(self):
         data = {'title': 'Post test title', 'content': 'Post test content'}
         url = api_reverse('api-reviews:review-cls')
         response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_get_item(self):
         game_review = GameReview.objects.first()
@@ -52,8 +56,54 @@ class GameReviewAPITestCase(APITestCase):
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code,
                          status.HTTP_405_METHOD_NOT_ALLOWED)
-
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, 
-                         status.HTTP_403_FORBIDDEN)
+                         status.HTTP_401_UNAUTHORIZED)
+
+    def test_update_item_with_user(self):
+        game_review = GameReview.objects.first()
+        url = game_review.get_api_url()
+        data = {'title': 'Updated test title',
+                'content': 'Updated test content'}
+        user_obj = User.objects.first()
+        payload = payload_handler(user_obj)
+        token_rsp = encode_handler(payload)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token_rsp)
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code,
+                         status.HTTP_200_OK)
+
+    def test_post_item_with_user(self):
+        data = {'title': 'Post test title', 'content': 'Post test content'}
+        url = api_reverse('api-reviews:review-cls')
+        user_obj = User.objects.first()
+        payload = payload_handler(user_obj)
+        token_rsp = encode_handler(payload)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token_rsp)
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_user_ownership(self):
+        owner = User(username='testuser2', email='test2@test.com')
+        owner.set_password('secret')
+        owner.save()
+        game_review = GameReview.objects.create(
+            user=owner,
+            title='testuser2 title',
+            content='Some testuser2 content'
+        )
+        
+        user_obj = User.objects.first()
+        self.assertNotEqual(user_obj.username, owner.username)
+
+        payload = payload_handler(user_obj)
+        token_rsp = encode_handler(payload)
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + token_rsp)
+
+        url = game_review.get_api_url()
+        data = {'title': 'Updated test title',
+                'content': 'Updated test content'}
+
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
